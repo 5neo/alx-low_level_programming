92a6a860c4bdc7e3441e00b0e956249a26ff5ba1
Walker <abdalqadermomen@gmail.com>
2023-08-10 08:30:12 +0300

    hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 
- /*
- * Write a C program that prints exactly:
- * \"Programming is like building a multilingual puzzle
- */
-
- /**
+/**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	/*main function*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
