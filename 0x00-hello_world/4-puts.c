1130558f80f86ccb425b9797edc0621c8834fb42
Walker <abdalqadermomen@gmail.com>
2023-08-10 08:06:08 +0300

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+ * Write a C program that prints exactly:
+ * "Programming is like building a multilingual puzzle
+ */
+
+/*the function returns 0*/
+int main()
+{
+	 puts("\"Programming is like building a multilingual puzzle");
+	 return 0;
+}
