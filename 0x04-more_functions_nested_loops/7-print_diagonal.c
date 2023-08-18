eb43a66a29d186de13a732db9f3c203d9609f39b
walker <abdalqadermomen@gmail.com>
2023-08-18 04:06:53 +0300

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -2,23 +2,23 @@
 
 /**
   * print_square - Prints n squares according n number of times
-  * @size: The number of squares/number of times
+  * @n: The number of squares/number of times
   *
   * Return: empty
   */
-void print_square(int size)
+void print_square(int n)
 {
 	int x, y;
 
-	if (size <= 0)
+	if (n <= 0)
 	{
 		putchar('\n');
 	}
 	else
 	{
-		for (x = 0; x < size; x++)
+		for (x = 0; x < n; x++)
 		{
-			for (y = 0; y < size; y++)
+			for (y = 0; y < n; y++)
 			{
 				putchar(35);
 			}
