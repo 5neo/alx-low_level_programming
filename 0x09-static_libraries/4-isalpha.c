cc9fa168465402897e3968baa286206e171d958a
Walker <abdalqadermomen@gmail.com>
2023-09-04 17:14:22 +0300

    add all files

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isalpha - check if the given charachter c is in lower case
+ * @c: character to be verified
+ *
+ * Return: 1 (True) else 0 (False)
+ */
+int _isalpha(char c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0);
+}
