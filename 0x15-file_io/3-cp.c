31d5ad79d9cc4892f9154c3ecaa75c0de67c43a5
Walker <abdalqadermomen@gmail.com>
2023-10-03 02:20:51 +0300

    copying

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,12 +3,12 @@
 
 /**
  * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
+ * @fileFrom: file_from.
+ * @fileTo: file_to.
  * @argv: arguments vector.
  * Return: no return.
  */
-void error_file(int file_from, int file_to, char *argv[])
+void error_file(int fileFrom, int fileTo, char *argv[])
 {
 	if (file_from == -1)
 	{
@@ -56,7 +56,7 @@ int main(int argc, char *argv[])
 	}
 
 	errClose = close(fileFrom);
-	if (err_close == -1)
+	if (errClose == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fileFrom);
 		exit(100);
