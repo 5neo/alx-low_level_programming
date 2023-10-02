b047a369552c5a6404045f3c78470dbe06f394fb
Walker <abdalqadermomen@gmail.com>
2023-10-03 02:05:20 +0300

    append text at end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,7 +21,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		for (nOfletters = 0; text_content[nletters]; nOfletters++)
+		for (nOfletters = 0; text_content[nOfletters]; nOfletters++)
 			;
 		rewr = write(filed, text_content, nOfletters);
 		if (rewr == -1)
